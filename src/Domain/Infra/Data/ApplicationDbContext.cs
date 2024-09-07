0b18a8d98b8db7f26f72371b314c34e56b354776
Elias Castro <38994457+EliasSarges@users.noreply.github.com>
2024-09-07 17:51:48 -0300

    feat: add application db context and some base types

---
+++ src/Domain/Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Domain.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(255);
+
+        builder.Entity<Product>()
+            .Property(p => p.Name)
+            .IsRequired();
+
+        builder.Entity<Category>()
+            .Property(p => p.Name)
+            .IsRequired();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder
+            .Properties<string>()
+            .HaveMaxLength(100);
+    }
+}
---
+++ src/Domain/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿using IWantApp.Domain;
+using IWantApp.Domain.Products;
+
+public class Product : Entity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+}
